024e631395d09dbc3a97c7018d9b914fbe0d874f
brijesh2004 <brskumar0102@gmail.com>
2023-03-29 13:50:02 +0530

    add backend

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 require("./db/conn");
+const PORT = process.env.PORT||7000
 
 app.use(express.json());
 app.use(require('./router/auth'));
@@ -10,7 +11,7 @@ app.get("/",(req , res)=>{
   res.send("Hello from the home side");
 })
 
-app.listen(7000 , ()=>{
-    console.log(`Listening on the port Number 7000`);
+app.listen(PORT , ()=>{
+    console.log(`Listening on the port Number ${PORT}`);
 })
 
