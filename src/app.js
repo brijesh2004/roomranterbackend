26e821a44cd41b75b8b824afaa97324fffb4af37
brijesh2004 <brskumar0102@gmail.com>
2023-03-31 21:49:52 +0530

    adding the fronted

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 require("./db/conn");
 const PORT = process.env.PORT||7000
@@ -6,7 +7,13 @@ const PORT = process.env.PORT||7000
 app.use(express.json());
 app.use(require('./router/auth'));
 
-
+app.use(
+  cors({
+    credentials:true,
+    origin:['https://master--ornate-kheer-5bbba7.netlify.app/'],
+    methods:['GET','POST'],
+  })
+)
 app.get("/",(req , res)=>{
   res.send("Hello from the home side");
 })
